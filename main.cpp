172dc921646df42aaf35938294b8c6dd094b4013
hasherezade <hasherezade@gmail.com>
2017-12-29 22:49:51 +0100

    Update libpeconv

--- main.cpp
+++ main.cpp
@@ -289,7 +289,7 @@ void banner(char *version)
 
 int main(int argc, char *argv[])
 {
-	char *version = "0.0.8.4";
+	char *version = "0.0.8.5";
 	if (argc < 2) {
 		banner(version);
 		system("pause");
