b2defd3cf35aef1f335f32672d64b5d656d6da29
hasherezade <hasherezade@gmail.com>
2017-12-30 02:22:53 +0100

    [REFACT] Normalized variable type

--- main.cpp
+++ main.cpp
@@ -330,7 +330,7 @@ int main(int argc, char *argv[])
 	args.filter = LIST_MODULES_ALL;
 
 	//Parse parameters
-	for (size_t i = 1; i < argc; i++) {
+	for (int i = 1; i < argc; i++) {
 		if (!strcmp(argv[i], PARAM_HELP) || !strcmp(argv[i], PARAM_HELP2)) {
 			print_help();
 			info_req = true;
