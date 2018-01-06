4d7721ab10c6464d212631704852273bd8ef6ed5
hasherezade <hasherezade@gmail.com>
2018-01-07 00:13:00 +0100

    [VERSION] 0.0.9.6

--- pe_sieve.h
+++ pe_sieve.h
@@ -7,7 +7,7 @@
 #include "scan_report.h"
 #include "report_formatter.h"
 
-static char VERSION[] = "0.0.9.5";
+static char VERSION[] = "0.0.9.6";
 static char URL[] = "https://github.com/hasherezade/pe-sieve";
 
 std::string info();
