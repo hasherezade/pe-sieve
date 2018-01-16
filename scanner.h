74e0c85e198f7b0b485120393da11cc1707b27c9
hasherezade <hasherezade@gmail.com>
2018-01-16 03:41:55 +0100

    [FEATURE] Scan working set for RWX pages

--- scanner.h
+++ scanner.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #include <Windows.h>
+#include <Psapi.h>
 #include <map>
 
 #include "peconv.h"
-
 #include "scan_report.h"
 
 class ModuleData {
@@ -64,6 +64,9 @@ public:
 	ProcessScanReport* scanRemote(); //throws exceptions
 
 protected:
+	ProcessScanReport* scanModules(ProcessScanReport *pReport); //throws exceptions
+	ProcessScanReport* scanWorkingSet(ProcessScanReport *pReport);
+
 	size_t enumModules(OUT HMODULE hMods[], IN const DWORD hModsMax, IN DWORD filters); //throws exceptions
 
 	t_scan_status scanForHollows(ModuleData& modData, ProcessScanReport& process_report);
