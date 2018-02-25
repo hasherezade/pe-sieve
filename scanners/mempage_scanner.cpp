270847d52a338623018c3e2c6302b426f68de1d4
hasherezade <hasherezade@gmail.com>
2018-02-25 05:31:34 +0100

    [REFACT] MemPageScanner cleanup

--- scanners/mempage_scanner.cpp
+++ scanners/mempage_scanner.cpp
@@ -1,69 +1,60 @@
 #include "mempage_scanner.h"
 
-//template <typename T_MEMORY_BASIC_INFORMATION>
-bool fill_page_info(HANDLE hProcess, MemPageData &memPageData)
-{
-	MEMORY_BASIC_INFORMATION page_info = { 0 };
-	SIZE_T out = VirtualQueryEx(hProcess, (LPCVOID) memPageData.start_va, &page_info, sizeof(page_info));
-	if (out != 0) {
-		memPageData.initial_protect = page_info.AllocationProtect;
-		memPageData.is_private = (page_info.Type == MEM_PRIVATE);
-		memPageData.protection = page_info.Protect;
-		return true;
-	}
-	if (memPageData.basic_protection == 0) { // accerss denied
-		return false;
-	}
-	std::cout << "info error: " << std::dec << GetLastError() << "basicp: " << memPageData.basic_protection << std::endl;
-	return false;
-}
-
 bool MemPageData::fillInfo()
 {
-	is_info_filled = false;
-	/*
-	BOOL is32b = TRUE;
-#ifdef _WIN64
-	IsWow64Process(this->processHandle, &is32b);
-	if (!is32b) {
-		is_info_filled = fill_page_info(this->processHandle, *this);
-		return is_info_filled;
+	MEMORY_BASIC_INFORMATION page_info = { 0 };
+	SIZE_T out = VirtualQueryEx(this->processHandle, (LPCVOID) start_va, &page_info, sizeof(page_info));
+	if (out != 0) {
+		initial_protect = page_info.AllocationProtect;
+		is_private = (page_info.Type == MEM_PRIVATE);
+		protection = page_info.Protect;
+		return true;
 	}
-#endif*/
-	is_info_filled = fill_page_info(this->processHandle, *this);
-	return is_info_filled;
+	if (basic_protection == 0) { // accerss denied
+		return false;
+	}
+	std::cout << "info error: " << std::dec << GetLastError() << "basicp: " << basic_protection << std::endl;
+	return false;
 }
 
 MemPageScanReport* MemPageScanner::scanRemote(MemPageData &memPage)
 {
+	bool only_executable = false; //scan only executable pages
+
 	if (!memPage.is_info_filled && !memPage.fillInfo()) {
 		return nullptr;
 	}
 	if (!memPage.is_private) {
 		return nullptr;
 	}
-	// is the page executable?
-	bool is_any_exec = (memPage.initial_protect & PAGE_EXECUTE_READWRITE)
-		|| (memPage.initial_protect & PAGE_EXECUTE)
-		|| (memPage.protection & PAGE_EXECUTE_READWRITE)
-		|| (memPage.protection & PAGE_EXECUTE);
-	
-	if ((memPage.protection & PAGE_EXECUTE_WRITECOPY) 
-		|| (memPage.protection == PAGE_READONLY)
-		)
-	{
+	if (memPage.protection & PAGE_EXECUTE_WRITECOPY) {
 		// they are probably legit
 		return nullptr;
 	}
+	// is the page executable?
+	bool is_any_exec = (memPage.initial_protect & PAGE_EXECUTE_READWRITE)
+		|| (memPage.initial_protect & PAGE_EXECUTE_READ)
+		|| (memPage.initial_protect & PAGE_EXECUTE)
+		|| (memPage.protection & PAGE_EXECUTE_READWRITE)
+		|| (memPage.protection & PAGE_EXECUTE_READ)
+		|| (memPage.initial_protect & PAGE_EXECUTE);
 
-	if (memPage.is_listed_module) {
-		std::cout << std::hex << memPage.start_va << "Aleady listed" << std::endl;
-	}
-	if (!is_any_exec && memPage.is_listed_module) {
-		//it was already scanned, probably not interesting
+	// sometimes non-executable pages can be used to re-map malware before loading into executable ones,
+	// so it is worth to check the non executables also
+	if (only_executable && !is_any_exec) {
+		// scanning only executable was enabled
 		return nullptr;
 	}
 
+	if (!is_any_exec && (memPage.protection == PAGE_READONLY)) {
+		// readonly, skip it
+		return nullptr;
+	}
+	if (!is_any_exec && memPage.is_listed_module) {
+		//it was already scanned, probably not interesting
+		std::cout << std::hex << memPage.start_va << "Aleady listed" << std::endl;
+		return nullptr;
+	}
 	static BYTE hdrs[peconv::MAX_HEADER_SIZE] = { 0 };
 	memset(hdrs, 0, peconv::MAX_HEADER_SIZE);
 	if (!peconv::read_remote_pe_header(this->processHandle,(BYTE*) memPage.start_va, hdrs, peconv::MAX_HEADER_SIZE)) {
