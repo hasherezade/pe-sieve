049469165f1b428486a3587bdc9059ace6d0b018
hasherezade <hasherezade@gmail.com>
2018-03-14 03:37:34 +0100

    [BUGFIX] In PatchAnalyzer: fixed opcode parsing (modifiers)

--- scanners/hook_scanner.cpp
+++ scanners/hook_scanner.cpp
@@ -82,13 +82,27 @@ size_t PatchAnalyzer::parseJmp(PatchList::Patch &patch, PBYTE patch_ptr, ULONGLO
 
 size_t PatchAnalyzer::parseMovJmp(PatchList::Patch &patch, PBYTE patch_ptr, bool is_long)
 {
-	size_t mov_instr_len = 5;
-	if (is_long) {
-		mov_instr_len = 9;
-	}
+	size_t mov_instr_len = is_long ? 9 : 5;
 	PBYTE jmp_ptr = patch_ptr + mov_instr_len; // next instruction
+
+	BYTE modifier0 = 0;
+	if (is64Modifier(*patch_ptr)) {
+		modifier0 = *patch_ptr;
+		patch_ptr++;
+		jmp_ptr++;
+		mov_instr_len++; // add length of modifier
+	}
+	
+	DWORD reg_id0 = patch_ptr[0] - 0xB8;
+
+	// before call/jmp there can be also the modifier...
+	BYTE modifier1 = 0;
+	if (is64Modifier(*jmp_ptr)) {
+		modifier1 = *jmp_ptr;
+		jmp_ptr++;
+		mov_instr_len++; // add length of modifier
+	}
 	DWORD reg_id1 = 0;
-	//TODO: before call/jmp there can be also the modifier...
 	if (jmp_ptr[0] == 0xFF && jmp_ptr[1] >= 0xE0 && jmp_ptr[1] <= 0xEF ) { // jmp reg
 		//jmp reg
 		reg_id1 = jmp_ptr[1] - 0xE0;
@@ -97,12 +111,12 @@ size_t PatchAnalyzer::parseMovJmp(PatchList::Patch &patch, PBYTE patch_ptr, bool
 		reg_id1 = jmp_ptr[1] - 0xD0;
 	} else {
 #ifdef _DEBUG
-		std::cerr << "It is not MOV->JMP" << std::endl;
+		std::cerr << "It is not MOV->JMP" << std::hex << (DWORD)jmp_ptr[0] << std::endl;
 #endif
 		return NULL;
 	}
-	DWORD reg_id2 = patch_ptr[0] - 0xB8;;
-	if (reg_id1 != reg_id2) {
+	//TODO: take into account also modifiers
+	if (reg_id1 != reg_id0) {
 #ifdef _DEBUG
 		std::cerr << "MOV->JMP : reg mismatch" << std::endl;
 #endif
@@ -114,12 +128,14 @@ size_t PatchAnalyzer::parseMovJmp(PatchList::Patch &patch, PBYTE patch_ptr, bool
 		DWORD *lval = (DWORD*)((ULONGLONG) patch_ptr + 1);
 		addr = *lval;
 	} else { //64bit
-		mov_instr_len++; // add length of modifier
 		ULONGLONG *lval = (ULONGLONG*)((ULONGLONG) patch_ptr + 1);
 		addr = *lval;
 	}
 	patch_size += 2; //add jump reg size
 	patch.setHookTarget(addr);
+#ifdef _DEBUG
+	std::cout << "----> Target: " << std::hex << addr << std::endl;
+#endif
 	return patch_size;
 }
 
@@ -136,6 +152,24 @@ size_t PatchAnalyzer::parsePushRet(PatchList::Patch &patch, PBYTE patch_ptr)
 	return instr_size;
 }
 
+bool PatchAnalyzer::is64Modifier(BYTE op)
+{
+	if (!isModule64bit) return false;
+	if (op >= 0x40 && op <= 0x4F) { // modifier
+		return true;
+	}
+	return false;
+}
+
+bool PatchAnalyzer::isLongModifier(BYTE op)
+{
+	if (!isModule64bit) return false;
+	if (op >= 0x48 && op <= 0x4F) { // modifier
+		return true;
+	}
+	return false;
+}
+
 size_t PatchAnalyzer::analyze(PatchList::Patch &patch)
 {
 	ULONGLONG section_va = moduleData.rvaToVa(sectionRVA);
@@ -150,14 +184,12 @@ size_t PatchAnalyzer::analyze(PatchList::Patch &patch)
 	if (op == OP_PUSH_DWORD) {
 		return parsePushRet(patch, patch_ptr);
 	}
-	bool is64bit = this->moduleData.is64bit();
 	bool is_long = false;
-	if (is64bit) {
-		if (op >= 0x40 && op <= 0x4F) { // mov modifier
-			patch_ptr++;
-			op = patch_ptr[0];
+	if (is64Modifier(op)) { // mov modifier
+		if (isLongModifier(op)) {
 			is_long = true;
 		}
+		op = patch_ptr[1];
 	}
 	if (op >= 0xB8 && op <= 0xBF) { // is mov
 		return parseMovJmp(patch, patch_ptr, is_long);
--- scanners/hook_scanner.h
+++ scanners/hook_scanner.h
@@ -81,6 +81,7 @@ public:
 	PatchAnalyzer(ModuleData &_moduleData,DWORD _sectionRVA, PBYTE patched_code, size_t code_size)
 		: moduleData(_moduleData), sectionRVA(_sectionRVA), patchedCode(patched_code), codeSize(code_size)
 	{
+		isModule64bit = moduleData.is64bit();
 	}
 
 	size_t analyze(PatchList::Patch &patch);
@@ -89,7 +90,12 @@ protected:
 	size_t parseJmp(PatchList::Patch &patch, PBYTE patch_ptr, ULONGLONG patch_va);
 	size_t parseMovJmp(PatchList::Patch &patch, PBYTE patch_ptr,bool is_long);
 	size_t parsePushRet(PatchList::Patch &patch, PBYTE patch_ptr);
+
 	ULONGLONG PatchAnalyzer::getJmpDestAddr(ULONGLONG currVA, DWORD instrLen, DWORD lVal);
+	bool is64Modifier(BYTE op);
+	bool isLongModifier(BYTE op);
+
+	bool isModule64bit;
 
 	ModuleData &moduleData;
 	DWORD sectionRVA;
