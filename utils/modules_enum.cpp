58a7476ee44df0f0463a5015fb36010602de4d5a
hasherezade <hasherezade@gmail.com>
2018-04-12 18:54:47 +0200

    [REFACT] In 32bit build use EnumProcessModules (not EnumProcessModulesEx) - workaround for Issue #11 (to build for 32bit XP)

--- utils/modules_enum.cpp
+++ utils/modules_enum.cpp
@@ -8,10 +8,17 @@ size_t enum_modules(IN HANDLE hProcess, IN OUT HMODULE hMods[], IN const DWORD h
 	if (hProcess == nullptr) return 0;
 
 	DWORD cbNeeded;
+#ifdef _WIN64
 	if (!EnumProcessModulesEx(hProcess, hMods, hModsMax, &cbNeeded, filters)) {
 		throw std::exception("Could not enumerate modules in the process. ", GetLastError());
 		return 0;
 	}
+#else
+	if (!EnumProcessModules(hProcess, hMods, hModsMax, &cbNeeded)) {
+		throw std::exception("Could not enumerate modules in the process. ", GetLastError());
+		return 0;
+	}
+#endif
 	const size_t modules_count = cbNeeded / sizeof(HMODULE);
 	return modules_count;
 }
