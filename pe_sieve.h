fee430405f65b40ad9369ea63015d2d113dc8529
hasherezade <hasherezade@gmail.com>
2018-11-14 00:43:20 +0100

    [BUGFIX] Updated libpeconv: fixed bug in imports recovery (names/ordinals written to Thunk instead of OriginalThunk)

--- pe_sieve.h
+++ pe_sieve.h
@@ -7,8 +7,8 @@
 #include "scanners/scan_report.h"
 #include "report_formatter.h"
 
-static char PESIEVE_VERSION[] = "0.1.5.1";
-static DWORD PESIEVE_VERSION_ID = 0x00010501; // 00 01 05 00
+static char PESIEVE_VERSION[] = "0.1.5.2";
+static DWORD PESIEVE_VERSION_ID = 0x00010502; // 00 01 05 02
 static char PESIEVE_URL[] = "https://github.com/hasherezade/pe-sieve";
 
 std::string info();
