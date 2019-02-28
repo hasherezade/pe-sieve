fb8df4adb26cfa01cc408224d385d345535c93cf
hasherezade <hasherezade@gmail.com>
2019-02-28 23:11:20 +0100

    [BUGFIX] Updated libpeconv with a bugfix (Issue #29)

--- pe_sieve.h
+++ pe_sieve.h
@@ -8,7 +8,7 @@
 #include "scanners/scan_report.h"
 #include "postprocessors/report_formatter.h"
 
-static char PESIEVE_VERSION[] = "0.1.6.1";
+static char PESIEVE_VERSION[] = "0.1.6.2";
 static DWORD PESIEVE_VERSION_ID = 0x00010600; // 00 01 06 00
 static char PESIEVE_URL[] = "https://github.com/hasherezade/pe-sieve";
 
