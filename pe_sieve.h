86e81e48d845d85eee1ef701e763288b33f755f8
hasherezade <hasherezade@gmail.com>
2019-11-14 16:16:36 +0100

    [VERSION] 0.2.3.4

--- pe_sieve.h
+++ pe_sieve.h
@@ -8,8 +8,8 @@
 #include "scanners/scan_report.h"
 #include "postprocessors/report_formatter.h"
 
-static char PESIEVE_VERSION[] = "0.2.3.2";
-static DWORD PESIEVE_VERSION_ID = 0x00020302; // 00 02 03 02
+static char PESIEVE_VERSION[] = "0.2.3.4";
+static DWORD PESIEVE_VERSION_ID = 0x00020304; // 00 02 03 04
 static char PESIEVE_URL[] = "https://github.com/hasherezade/pe-sieve";
 
 std::string info();
