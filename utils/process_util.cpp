aa37d22b2177e788f51e2b614f04347991ecc24f
hasherezade <hasherezade@gmail.com>
2019-11-16 06:52:04 +0100

    [REFACT] In process_util: load functions only once

--- utils/process_util.cpp
+++ utils/process_util.cpp
@@ -1,10 +1,15 @@
 #include "process_util.h"
+#include <iostream>
 
 HMODULE g_kernel32Hndl = nullptr;
 
+BOOL (WINAPI *g_IsWow64Process)(IN HANDLE, OUT PBOOL) = nullptr;
+BOOL (WINAPI *g_Wow64DisableWow64FsRedirection) (OUT PVOID* OldValue) = nullptr;
+BOOL (WINAPI *g_Wow64RevertWow64FsRedirection) (IN PVOID OldValue) = nullptr;
+
 HMODULE get_kernel32_hndl()
 {
-	if (!g_kernel32Hndl) {
+	if (g_kernel32Hndl == nullptr) {
 		g_kernel32Hndl = LoadLibraryA("kernel32.dll");
 	}
 	return g_kernel32Hndl;
@@ -15,37 +20,51 @@ BOOL is_process_wow64(IN HANDLE processHandle, OUT BOOL* isProcWow64)
 	if (isProcWow64) {
 		(*isProcWow64) = FALSE; //set default output value: FALSE
 	}
+	if (!g_IsWow64Process) {
+		HMODULE kernelLib = get_kernel32_hndl();
+		if (!kernelLib) return FALSE;
 
-	HMODULE kernelLib = get_kernel32_hndl();
-	if (!kernelLib) return FALSE;
+		FARPROC procPtr = GetProcAddress(kernelLib, "IsWow64Process");
+		if (!procPtr) return FALSE;
 
-	FARPROC procPtr = GetProcAddress(kernelLib, "IsWow64Process");
-	if (!procPtr) return FALSE;
-
-	BOOL (WINAPI *_IsWow64Process)(IN HANDLE, OUT PBOOL) = (BOOL (WINAPI *)(IN HANDLE, OUT PBOOL)) procPtr;
-	return _IsWow64Process(processHandle, isProcWow64);
+		g_IsWow64Process = (BOOL(WINAPI *)(IN HANDLE, OUT PBOOL))procPtr;
+	}
+	if (!g_IsWow64Process) {
+		return FALSE;
+	}
+	return g_IsWow64Process(processHandle, isProcWow64);
 }
 
 BOOL wow64_disable_fs_redirection(OUT PVOID* OldValue)
 {
-	HMODULE kernelLib = get_kernel32_hndl();
-	if (!kernelLib) return FALSE;
+	if (!g_Wow64DisableWow64FsRedirection) {
+		HMODULE kernelLib = get_kernel32_hndl();
+		if (!kernelLib) return FALSE;
 
-	FARPROC procPtr = GetProcAddress(kernelLib, "Wow64DisableWow64FsRedirection");
-	if (!procPtr) return FALSE;
+		FARPROC procPtr = GetProcAddress(kernelLib, "Wow64DisableWow64FsRedirection");
+		if (!procPtr) return FALSE;
 
-	BOOL(WINAPI *_Wow64DisableWow64FsRedirection) (OUT PVOID* OldValue) = (BOOL(WINAPI *) (OUT PVOID* ))procPtr;
-	return _Wow64DisableWow64FsRedirection(OldValue);
+		g_Wow64DisableWow64FsRedirection = (BOOL(WINAPI *) (OUT PVOID*))procPtr;
+	}
+	if (!g_Wow64DisableWow64FsRedirection) {
+		return FALSE;
+	}
+	return g_Wow64DisableWow64FsRedirection(OldValue);
 }
 
 BOOL wow64_revert_fs_redirection(IN PVOID OldValue)
 {
-	HMODULE kernelLib = get_kernel32_hndl();
-	if (!kernelLib) return FALSE;
+	if (!g_Wow64RevertWow64FsRedirection) {
+		HMODULE kernelLib = get_kernel32_hndl();
+		if (!kernelLib) return FALSE;
 
-	FARPROC procPtr = GetProcAddress(kernelLib, "Wow64RevertWow64FsRedirection");
-	if (!procPtr) return FALSE;
+		FARPROC procPtr = GetProcAddress(kernelLib, "Wow64RevertWow64FsRedirection");
+		if (!procPtr) return FALSE;
 
-	BOOL(WINAPI *_Wow64RevertWow64FsRedirection) (IN PVOID OldValue) = (BOOL(WINAPI *) (IN PVOID))procPtr;
-	return _Wow64RevertWow64FsRedirection(OldValue);
+		g_Wow64RevertWow64FsRedirection = (BOOL(WINAPI *) (IN PVOID))procPtr;
+	}
+	if (!g_Wow64RevertWow64FsRedirection) {
+		return FALSE;
+	}
+	return g_Wow64RevertWow64FsRedirection(OldValue);
 }
