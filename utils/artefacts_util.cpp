70ee8a9a02cfccd85780559044670bda4f5607c8
hasherezade <hasherezade@gmail.com>
2020-02-26 21:01:55 +0100

    [FEATURE] Added more shellcode detection patterns (Issue #50)

--- utils/artefacts_util.cpp
+++ utils/artefacts_util.cpp
@@ -27,11 +27,19 @@ bool is_32bit_code(BYTE *loadedData, size_t loadedSize)
 		0x89, 0xE5 // MOV EBP, ESP
 	};
 
+	BYTE prolog32_3_pattern[] = {
+		0x60, // PUSHAD
+		0x89, 0xE5 // MOV EBP, ESP
+	};
+
 	bool pattern_found = false;
 	if (find_pattern(loadedData, loadedSize, prolog32_pattern, sizeof(prolog32_pattern))) {
 		pattern_found = true;
 	}
 	else if (find_pattern(loadedData, loadedSize, prolog32_2_pattern, sizeof(prolog32_2_pattern))) {
+		pattern_found = true;
+	}
+	else if (find_pattern(loadedData, loadedSize, prolog32_3_pattern, sizeof(prolog32_3_pattern))) {
 		pattern_found = true;
 	}
 	return pattern_found;
@@ -43,8 +51,22 @@ bool is_64bit_code(BYTE *loadedData, size_t loadedSize)
 		0x40, 0x53, // PUSH RBX
 		0x48, 0x83, 0xEC // SUB RSP, ??
 	};
+	BYTE prolog64_2_pattern[] = {
+		0x55, // PUSH RBP
+		0x48, 0x8b, 0xEC // MOV RBP, RSP
+	};
+	BYTE prolog64_3_pattern[] = {
+		0x40, 0x55, // PUSH ??
+		0x48, 0x83, 0xEC // SUB RSP, ??
+	};
 	bool pattern_found = false;
 	if (find_pattern(loadedData, loadedSize, prolog64_pattern, sizeof(prolog64_pattern))) {
+		pattern_found = true;
+	}
+	else if (find_pattern(loadedData, loadedSize, prolog64_2_pattern, sizeof(prolog64_2_pattern))) {
+		pattern_found = true;
+	}
+	else if (find_pattern(loadedData, loadedSize, prolog64_3_pattern, sizeof(prolog64_3_pattern))) {
 		pattern_found = true;
 	}
 	return pattern_found;
