ab328145aa9b5fa58000222195b152bd592cf098
hasherezade <hasherezade@gmail.com>
2020-06-06 10:06:34 +0200

    [BUGFIX] Removed upper limit for scanned page address

--- include/pe_sieve_version.h
+++ include/pe_sieve_version.h
@@ -6,8 +6,8 @@
 namespace pesieve {
 #endif
 
-	static char PESIEVE_VERSION[] = "0.2.6.1";
-	static DWORD PESIEVE_VERSION_ID = 0x00020601; // 00 02 06 01
+	static char PESIEVE_VERSION[] = "0.2.6.2";
+	static DWORD PESIEVE_VERSION_ID = 0x00020602; // 00 02 06 02
 	static char PESIEVE_URL[] = "https://github.com/hasherezade/pe-sieve";
 
 #ifdef __cplusplus
--- utils/workingset_enum.cpp
+++ utils/workingset_enum.cpp
@@ -5,30 +5,41 @@
 namespace pesieve {
 	namespace util {
 
-		const ULONGLONG MAX_32BIT = 0x7FFFFFFFF;
-		const ULONGLONG MAX_64BIT = 0x7FFFFFFFFFFF;
-
-		bool get_next_region(HANDLE processHandle, ULONGLONG start_va, ULONGLONG max_va, MEMORY_BASIC_INFORMATION &page_info)
+		bool get_next_commited_region(HANDLE processHandle, ULONGLONG start_va, MEMORY_BASIC_INFORMATION &page_info)
 		{
-			for (; start_va < max_va; start_va += PAGE_SIZE) {
+			while (true) {
 				//std::cout << "Checking: " << std::hex << start_va << " vs " << std::hex << max_va << std::endl;
+				memset(&page_info, 0, sizeof(MEMORY_BASIC_INFORMATION));
 				SIZE_T out = VirtualQueryEx(processHandle, (LPCVOID)start_va, &page_info, sizeof(page_info));
-				if (out != sizeof(page_info)) {
-					const DWORD error = GetLastError();
-					if (error == ERROR_INVALID_PARAMETER) {
-						//nothing more to read
-						break;
-					}
-					if (error == ERROR_ACCESS_DENIED) {
-						std::cerr << "[WARNING] Cannot query the memory region. Error:" << std::dec << error << std::endl;
-						break;
-					}
+				const DWORD error = GetLastError();
+				if (error == ERROR_INVALID_PARAMETER) {
+					//nothing more to read
+#ifdef _DEBUG
+					std::cout << "Nothing more to read: " << std::hex << start_va << std::endl;
+#endif
+					break;
+				}
+				if (error == ERROR_ACCESS_DENIED) {
 					std::cerr << "[WARNING] Cannot query the memory region. Error:" << std::dec << error << std::endl;
+					break;
+				}
+				if (out != sizeof(page_info)) {
+					std::cerr << "[WARNING] Cannot query the memory region. Error:" << std::dec << error << std::endl;
+					start_va += PAGE_SIZE;
 					continue;
+				}
+				if ((page_info.State & MEM_FREE) || (page_info.State & MEM_COMMIT) == 0) {
+					if (page_info.RegionSize != 0) {
+						//std::cout << "Free:  " << std::hex << start_va << " RegionSize:" << page_info.RegionSize << std::endl;
+						start_va += page_info.RegionSize;
+						continue;
+					}
 				}
 				if (page_info.RegionSize == 0) {
+					start_va += PAGE_SIZE;
 					continue;
 				}
+				//std::cout << "Commited:  " << std::hex << start_va << " RegionSize:" << page_info.RegionSize << std::endl;
 				return true;
 			}
 			return false;
@@ -40,29 +51,20 @@ namespace pesieve {
 size_t pesieve::util::enum_workingset(HANDLE processHandle, std::set<ULONGLONG> &region_bases)
 {
 #ifdef _WIN64
-	ULONGLONG max_addr = MAX_64BIT;
+	ULONGLONG mask = ULONGLONG(-1);
 #else
-	ULONGLONG max_addr = MAX_32BIT;
+	ULONGLONG mask = DWORD(-1);
 #endif
 
 	size_t added = 0;
-	for (ULONGLONG next_va = 0; next_va <= max_addr; )
+	ULONGLONG next_va = 0;
+
+	MEMORY_BASIC_INFORMATION page_info = { 0 };
+	while (get_next_commited_region(processHandle, next_va, page_info))
 	{
-		MEMORY_BASIC_INFORMATION page_info = { 0 };
-		if (!get_next_region(processHandle, next_va, max_addr, page_info)) {
-			break;
-		}
-		//std::cout << "Got addr: " << std::hex << page_info.BaseAddress << std::endl;
-		
-		ULONGLONG base = (ULONGLONG) page_info.BaseAddress;
+		ULONGLONG base = (ULONGLONG)page_info.BaseAddress & mask;
 		next_va = base + page_info.RegionSize; //end of the region
 
-		if (page_info.State & MEM_FREE) continue;
-
-		if ((page_info.State & MEM_COMMIT) == 0) {
-			//skip pages that are not commited
-			continue;
-		}
 		region_bases.insert(base);
 		added++;
 	}
