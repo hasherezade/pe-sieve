e06bc8851ce1f360210bc429a0e750107fb92479
hasherezade <hasherezade@gmail.com>
2020-06-11 16:51:45 +0200

    [VERSION] 0.2.6.4

--- include/pe_sieve_version.h
+++ include/pe_sieve_version.h
@@ -6,8 +6,8 @@
 namespace pesieve {
 #endif
 
-	static char PESIEVE_VERSION[] = "0.2.6.3";
-	static DWORD PESIEVE_VERSION_ID = 0x00020603; // 00 02 06 03
+	static char PESIEVE_VERSION[] = "0.2.6.4";
+	static DWORD PESIEVE_VERSION_ID = 0x00020604; // 00 02 06 04
 	static char PESIEVE_URL[] = "https://github.com/hasherezade/pe-sieve";
 
 #ifdef __cplusplus
