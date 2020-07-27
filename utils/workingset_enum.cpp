1bc4dda285a1b0b95fd7056ac4e8b629a6f81c09
hasherezade <hasherezade@gmail.com>
2020-07-27 02:08:44 +0200

    [BUGFIX] Break if ERROR_BAD_LENGTH and out_size different than expected

--- utils/workingset_enum.cpp
+++ utils/workingset_enum.cpp
@@ -34,8 +34,15 @@ namespace pesieve {
 				(ERROR_BAD_LENGTH may occur if the scanner is 32 bit and running on a 64 bit system.)
 				Otherwise - also on different error - skip.
 				*/
-				if (out != sizeof(page_info) || error != ERROR_BAD_LENGTH) {
-					std::cerr << "[WARNING] Cannot query the memory region. Error: " << std::dec << error << std::endl;
+				if (error == ERROR_BAD_LENGTH) {
+					if (out != sizeof(page_info)) {
+						std::cerr << "[WARNING] Cannot query the memory region. Error: " << std::dec << error << std::endl;
+						break;
+					}
+					// if (out == sizeof(page_info) && error == ERROR_BAD_LENGTH), continue normally
+				} else {
+					// on any other error, skip a page:
+					std::cerr << "[WARNING] Skipping a page! Error: " << std::dec << error << std::endl;
 					start_va += PAGE_SIZE;
 					continue;
 				}
