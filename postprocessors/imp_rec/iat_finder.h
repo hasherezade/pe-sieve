48738a3f4883652611e47121817f5b884e1cdca4
hasherezade <hasherezade@gmail.com>
2020-07-28 04:06:44 +0200

    [REFACT] Normalize integer type

--- postprocessors/imp_rec/iat_finder.h
+++ postprocessors/imp_rec/iat_finder.h
@@ -44,7 +44,7 @@ namespace pesieve {
 			if (!exp) break;
 
 			is_terminated = false;
-			DWORD offset = ((BYTE*)imp - vBuf);
+			DWORD offset = MASK_TO_DWORD((BYTE*)imp - vBuf);
 			iat.append(offset, *imp, exp);
 
 			if (!series) series = new IATThunksSeries(offset);
