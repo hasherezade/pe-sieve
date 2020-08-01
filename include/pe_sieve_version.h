f70463842a95efe096279ac2396377ca76bbc0aa
hasherezade <hasherezade@gmail.com>
2020-08-01 12:38:13 +0200

    [BUGFIX] Updated libpeconv with a bugfix (do not treat empty reloc blocks as invalid)

--- include/pe_sieve_version.h
+++ include/pe_sieve_version.h
@@ -6,7 +6,7 @@
 namespace pesieve {
 #endif
 
-	const char PESIEVE_VERSION[] = "0.2.8.6";
+	const char PESIEVE_VERSION[] = "0.2.8.6-a";
 	const DWORD PESIEVE_VERSION_ID = 0x00020806; // 00 02 08 06
 	const char PESIEVE_URL[] = "https://github.com/hasherezade/pe-sieve";
 
