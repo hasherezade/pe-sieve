3784d2926225a385371e7094d686d84fbc6f33a2
hasherezade <hasherezade@gmail.com>
2021-05-07 16:09:12 +0200

    [BUGFIX] Fixed: not validated pointer in ExportMapper (Issue #84)

--- include/pe_sieve_version.h
+++ include/pe_sieve_version.h
@@ -6,8 +6,8 @@
 namespace pesieve {
 #endif
 
-	const char PESIEVE_VERSION[] = "0.2.9.5";
-	const DWORD PESIEVE_VERSION_ID = 0x00020905; // 00 02 09 05
+	const char PESIEVE_VERSION[] = "0.2.9.6";
+	const DWORD PESIEVE_VERSION_ID = 0x00020906; // 00 02 09 06
 	const char PESIEVE_URL[] = "https://github.com/hasherezade/pe-sieve";
 
 #ifdef __cplusplus
