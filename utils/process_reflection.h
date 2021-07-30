af2625c6e0d725eec156d8a5d804b2014807d061
hasherezade <hasherezade@gmail.com>
2021-07-30 18:32:03 +0200

    [REFACT] Removed not needed PssCaptureSnapshot flags

--- utils/process_reflection.h
+++ utils/process_reflection.h
@@ -8,10 +8,16 @@
 namespace pesieve {
 	namespace util {
 
+		// required by RtlCreateProcessReflection:
+		const DWORD reflection_access1 = PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION | PROCESS_DUP_HANDLE;
+
+		// required by PssCaptureSnapshot:
+		const DWORD reflection_access2 = PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION | PROCESS_DUP_HANDLE | PROCESS_CREATE_PROCESS;
+
 #ifdef USE_PROCESS_SNAPSHOT
-		const DWORD reflection_access = PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION | PROCESS_DUP_HANDLE | PROCESS_CREATE_PROCESS;
+		const DWORD reflection_access = reflection_access2;
 #else
-		const DWORD reflection_access = PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION | PROCESS_DUP_HANDLE;
+		const DWORD reflection_access = reflection_access1;
 #endif
 
 		bool can_make_process_reflection();
