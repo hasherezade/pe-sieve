cf17e445eec75e96bd8feda364147a644723431d
hasherezade <hasherezade@gmail.com>
2021-08-02 12:26:48 +0200

    [NOBIN] Fixed a typo in a comment. Added more comments.

--- scanners/hook_targets_resolver.h
+++ scanners/hook_targets_resolver.h
@@ -5,7 +5,7 @@
 
 namespace pesieve {
 
-	//!  Process the list of collected patches (preprocessed by PatchAnalyzer), and for those of them that were detected as hooks, it resolves information to which modules do they lead to.
+	//!  Processes the list of the collected patches (preprocessed by PatchAnalyzer), and for those of them that were detected as hooks, it resolves information about to which modules do they lead to.
 	class HookTargetResolver
 	{
 	public:
@@ -14,8 +14,11 @@ namespace pesieve {
 		{
 		}
 
+		//!  Resolves all the hooks collected within the given set of reports
 		size_t resolveAllHooks(const std::set<ModuleScanReport*> &code_reports);
-		bool resolveTarget(PatchList::Patch* currPatch);
+
+		//!  Resolves the information about the target of the provided hook, and fills it back into the object.
+		bool resolveTarget(IN OUT PatchList::Patch* currPatch);
 
 	protected:
 		ProcessScanReport& processReport;
