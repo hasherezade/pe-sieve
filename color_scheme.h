aed9c3ce200e22e0096f50dfc3429c4ae2859254
hasherezade <hasherezade@gmail.com>
2021-09-07 11:37:42 +0200

    [REFACT] Removed unused colors from color_scheme

--- color_scheme.h
+++ color_scheme.h
@@ -5,7 +5,4 @@ namespace pesieve {
 	const WORD ERROR_COLOR = 0x0c;
 	const WORD WARNING_COLOR = 0x0c;
 	const WORD HILIGHTED_COLOR = 0x0f;
-	const WORD HEADER_COLOR = 14;
-	const WORD SEPARATOR_COLOR = 6;
-	const WORD INACTIVE_COLOR = 8;
 };
