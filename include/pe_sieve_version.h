6999564e4506d524ca3f896a8d85755916cd5038
hasherezade <hasherezade@gmail.com>
2021-12-23 07:46:45 -0800

    [VERSION] 0.3.1.5

--- include/pe_sieve_version.h
+++ include/pe_sieve_version.h
@@ -6,8 +6,8 @@
 namespace pesieve {
 #endif
 
-	const char PESIEVE_VERSION[] = "0.3.1.3";
-	const DWORD PESIEVE_VERSION_ID = 0x00030103; // 00 03 01 03
+	const char PESIEVE_VERSION[] = "0.3.1.5";
+	const DWORD PESIEVE_VERSION_ID = 0x00030105; // 00 03 01 05
 	const char PESIEVE_URL[] = "https://github.com/hasherezade/pe-sieve";
 
 #ifdef __cplusplus
