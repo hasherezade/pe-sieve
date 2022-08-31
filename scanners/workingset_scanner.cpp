690db9aea528eb6635914371d20130d218941856
hasherezade <hasherezade@gmail.com>
2022-08-30 17:17:43 -0700

    [FEATURE] In WorkingSet scanner: added checks if the scanned page has the same bounds as the previously collected

--- scanners/workingset_scanner.cpp
+++ scanners/workingset_scanner.cpp
@@ -165,6 +165,16 @@ WorkingSetScanReport* pesieve::WorkingSetScanner::scanRemote()
 #endif
 		return nullptr;
 	}
+	// sanity checks to make sure that we are scanning the same page that was previously collected:
+	if (memPage.alloc_base != this->memRegion.alloc_base) {
+		std::cerr << "WARNING: Alloc Base mismatch: " << std::hex << memPage.alloc_base << " vs " << this->memRegion.alloc_base << std::endl;
+		return nullptr;
+	}
+	if ((memPage.region_end - memPage.region_start) != this->memRegion.size) {
+		std::cerr << "WARNING: Size mismatch: " << std::hex << (memPage.region_end - memPage.region_start) << " vs " << this->memRegion.size << std::endl;
+		return nullptr;
+	}
+
 	// is the page executable?
 	const bool is_any_exec = isExecutable(memPage);
 	if (!is_any_exec) {
