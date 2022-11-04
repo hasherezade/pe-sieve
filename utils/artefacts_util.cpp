aa06ceba94926eb08f20dfd273b201253b10d997
hasherezade <hasherezade@gmail.com>
2022-11-04 08:22:53 -0700

    [FEATURE] Extended prolog pattern to avoid false positives (Issue #108)

--- utils/artefacts_util.cpp
+++ utils/artefacts_util.cpp
@@ -83,6 +83,11 @@ bool pesieve::util::is_64bit_code(BYTE *loadedData, size_t loadedSize)
 		0x48, 0x89, 0xE7 // MOV RDI, RSP
 	};
 	BYTE prolog64_7_pattern[] = {
+		 0x48, 0x8B, 0xC4, // MOV RAX, RSP
+		 0x48, 0x89, 0x58, 0x08, // MOV QWORD PTR [RAX + 8], RBX
+		 0x4C, 0x89, 0x48, 0x20, // MOV QWORD PTR [RAX + 0X20], R9
+		 0x4C, 0x89, 0x40, 0x18, // MOV QWORD PTR [RAX + 0X18], R8
+		 0x48, 0x89, 0x50, 0x10, // MOV QWORD PTR [RAX + 0X10], RDX
 		 0x55, // PUSH RBP
 		 0x56, // PUSH RSI
 		 0x57, // PUSH RDI 
