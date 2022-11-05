d01de99f931151d82987fba5d1175b78906b73fa
hasherezade <hasherezade@gmail.com>
2022-11-05 09:07:10 -0700

    [FEATURE] End saved reports with a newline

--- postprocessors/results_dumper.cpp
+++ postprocessors/results_dumper.cpp
@@ -137,7 +137,7 @@ bool pesieve::ResultsDumper::dumpJsonReport(pesieve::ProcessScanReport &process_
 	if (json_report.is_open() == false) {
 		return false;
 	}
-	json_report << report_all;
+	json_report << report_all << std::endl;
 	if (json_report.is_open()) {
 		json_report.close();
 		return true;
@@ -166,7 +166,7 @@ bool pesieve::ResultsDumper::dumpJsonReport(ProcessDumpReport &process_report)
 	if (json_report.is_open() == false) {
 		return false;
 	}
-	json_report << report_all;
+	json_report << report_all << std::endl;
 	if (json_report.is_open()) {
 		json_report.close();
 		return true;
