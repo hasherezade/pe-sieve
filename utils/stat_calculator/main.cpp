4e3cf28c7f16a29588406d8a6a1f4f2e9d2a6a28
hasherezade <hasherezade@gmail.com>
2023-01-09 09:34:08 -0800

    [FEATURE] Added stats_calculator (a tool for tests)

---
+++ utils/stat_calculator/main.cpp
@@ -0,0 +1,76 @@
+#include <windows.h>
+#include <iostream>
+#include "../stats_analyzer.h"
+
+using namespace pesieve::util;
+
+struct Buffer
+{
+    Buffer()
+        : data(nullptr), data_size(0) {}
+
+    bool load(char* filename)
+    {
+        if (data) {
+            return false; // already filled
+        }
+        FILE* fp = fopen(filename, "rb");
+        if (!fp) return false;
+
+        fseek(fp, 0, SEEK_END);
+        size_t file_size = ftell(fp);
+        fseek(fp, 0, SEEK_SET);
+
+        data = (BYTE*)calloc(file_size, sizeof(BYTE));
+        if (data) {
+            data_size = fread(data, sizeof(BYTE), file_size, fp);
+        }
+        fclose(fp);
+        if (data_size) {
+            return true;
+        }
+        return false;
+    }
+
+    BYTE* data;
+    size_t data_size;
+};
+
+
+void printHistogram(ChunkStats<BYTE> currArea, std::stringstream& outs)
+{
+    for (auto itr = currArea.histogram.begin(); itr != currArea.histogram.end(); ++itr) {
+        outs << std::hex << std::setfill('0') << std::setw(2) << (UINT)itr->first
+            << " : " << IS_PRINTABLE(itr->first)
+            << " : " << std::dec << std::setfill(' ') << std::setw(5) << itr->second
+            << " : " << (double)itr->second / (double)currArea.size << "\n";
+    }
+}
+
+int main(size_t argc, char* argv[])
+{
+    if (argc < 2) {
+        std::cout << "Supply a file to be analyzed!\n";
+        return 0;
+    }
+    Buffer buf;
+    if (!buf.load(argv[1])) {
+        std::cout << "Failed loading the file!\n";
+        return 0;
+    }
+
+    std::cout << "Ready!\n";
+
+    AreaStats<BYTE> stats;
+    AreaStatsCalculator<BYTE> calc(buf.data, buf.data_size);
+    if (calc.fill(stats)) {
+        std::cout << "Stats filled!\n";
+    }
+    std::stringstream outs;
+    stats.toJSON(outs, 0);
+    outs << "\n";
+    printHistogram(stats.currArea, outs);
+    std::cout << outs.str();
+
+    return 0;
+}
