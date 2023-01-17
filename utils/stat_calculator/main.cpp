0f145a8fde8851c0e26609685788418cc7b8b1f5
hasherezade <hasherezade@gmail.com>
2023-01-17 14:25:28 -0800

    [FEATURE] Added standard deviation calculator

--- utils/stat_calculator/main.cpp
+++ utils/stat_calculator/main.cpp
@@ -6,6 +6,7 @@
 #include "../stats.h"
 #include "../stats_analyzer.h"
 #include "../basic_buffer.h"
+#include "../std_dev_calc.h"
 
 using namespace pesieve::util;
 
@@ -66,14 +67,16 @@ void printHistogram(ChunkStats<BYTE> &currArea, std::stringstream& outs)
 }
 
 
-void printFrequencies(ChunkStats<BYTE>& currArea, std::stringstream& outs)
+void printFrequencies(ChunkStats<BYTE>& currArea,  std::stringstream& outs, size_t max = 0)
 {
     if (!currArea.size) return;
 
-    size_t max = 10;
-    outs << "Top "<< max <<" Frequencies:\n";
     size_t i = 0;
-    for (auto itr1 = currArea.frequencies.rbegin(); itr1 != currArea.frequencies.rend() && i < max; ++itr1, ++i) {
+    for (auto itr1 = currArea.frequencies.rbegin(); itr1 != currArea.frequencies.rend(); ++itr1, ++i) {
+        if (max && i == max) break;
+        if (i % 10 == 0) {
+            outs << "Top " << (i / 10 + 1) *10 << " Frequencies:\n";
+        }
         std::set<BYTE> vals = itr1->second;
         size_t counter = itr1->first;
         for (auto itr2 = vals.begin(); itr2 != vals.end(); ++itr2) {
@@ -84,6 +87,33 @@ void printFrequencies(ChunkStats<BYTE>& currArea, std::stringstream& outs)
                 << " : " << (double)counter / (double)currArea.size << "\n";
         }
     }
+}
+
+size_t printPeakValues(ChunkStats<BYTE>& currArea, std::stringstream& outs, double stdDev)
+{
+    if (!currArea.size) return 0;
+
+    size_t peaksCount = 0;
+    outs << "Peak values\n";
+    size_t peakVal = currArea.frequencies.rbegin()->first;
+    size_t i = 0;
+    for (auto itr1 = currArea.frequencies.rbegin(); itr1 != currArea.frequencies.rend(); ++itr1, ++i) {
+        size_t counter = itr1->first;
+        double diff = (double)peakVal - (double)counter;
+        if (diff >  (3 * stdDev)) break;
+        
+        std::set<BYTE> vals = itr1->second;
+        peaksCount += vals.size();
+
+        for (auto itr2 = vals.begin(); itr2 != vals.end(); ++itr2) {
+            const BYTE val = *itr2;
+            outs << std::hex << std::setfill('0') << std::setw(2) << (UINT)val
+                << " : " << IS_PRINTABLE(val)
+                << " : " << std::dec << std::setfill(' ') << std::setw(5) << counter
+                << " : " << (double)counter / (double)currArea.size << "\n";
+        }
+    }
+    return peaksCount;
 }
 
 void printStrings(ChunkStats<BYTE> &currArea, std::stringstream& outs)
@@ -129,8 +159,8 @@ int main(size_t argc, char* argv[])
     printFrequencies(stats.currArea, outs);
     outs << "\n---\n";
     
-    printStrings(stats.currArea, outs);
-    outs << "\n---\n";
+    //printStrings(stats.currArea, outs);
+    //outs << "\n---\n";
 
     AreaInfo info;
     RuleMatchersSet matcherSet(RULE_CODE | RULE_ENCRYPTED | RULE_OBFUSCATED);
@@ -140,9 +170,30 @@ int main(size_t argc, char* argv[])
     else {
         outs << "NOT Suspicious!\n";
     }
+
+    StdDeviationCalc dev(stats.currArea.histogram, stats.currArea.histogram.size());
+
+    size_t topVal = stats.currArea.frequencies.rbegin()->first;
+    size_t bottomVal = stats.currArea.frequencies.begin()->first;
+    double diff = topVal - bottomVal;
+    double stDev = dev.calcSampleStandardDeviation();
+
+    size_t peaks = printPeakValues(stats.currArea, outs, stDev);
+    outs << "Peaks count: " << peaks << "\n";
+    outs << "Peaks ratio: " <<(double) peaks / (double)stats.currArea.histogram.size() << "\n";
+    outs << "---\n";
+
     info.toJSON(outs, 0);
     outs << "---\n";
     std::cout << outs.str();
+
+    dev.printAll();
+    std::cout << "Top - Bottom diff\t: " << diff << " in std dev: " << diff/ stDev << "\n";
+    //std::cout << "Sample Standard Deviation ratio\t: " << stDev / stats.currArea.size << "\n";
+    std::cout << "---\n";
+    std::cout << "Char diversity: " << (double)stats.currArea.histogram.size() / (double)255 << "\n";
+    std::cout << "Highest freq: " << stats.currArea.frequencies.rbegin()->first << "\n";
+    std::cout << "Lowest freq: " << stats.currArea.frequencies.begin()->first << "\n";
     std::cout << "Distinct frequencies: " << stats.currArea.frequencies.size() << "\n";
     std::cout << "Real start: " << buf.real_start << "\n";
     std::cout << "Real end: " << buf.real_end << "\n";
