55a2d222d702c710d0373391c19e1184280f06c6
hasherezade <hasherezade@gmail.com>
2023-07-08 09:48:38 -0700

    [NOBIN] Normalized indentations

--- stats/multi_stats.h
+++ stats/multi_stats.h
@@ -10,226 +10,226 @@
 
 namespace pesieve {
 
-    //! Settings defining what type of stats should be collected.
-    class MultiStatsSettings : public StatsSettings
-    {
-    public:
-        MultiStatsSettings()
-        {
-        }
+	//! Settings defining what type of stats should be collected.
+	class MultiStatsSettings : public StatsSettings
+	{
+	public:
+		MultiStatsSettings()
+		{
+		}
 
-        // Copy constructor
-        MultiStatsSettings(const MultiStatsSettings& p1)
-            : watchedStrings(p1.watchedStrings)
-        {
-        }
+		// Copy constructor
+		MultiStatsSettings(const MultiStatsSettings& p1)
+			: watchedStrings(p1.watchedStrings)
+		{
+		}
 
-        //! Searches a given substring among the `watchedStrings`. If the substring found, return the corresponding watched string containing the substring. 
-        std::string hasWatchedSubstring(std::string& lastStr)
-        {
-            for (auto itr = watchedStrings.begin(); itr != watchedStrings.end(); ++itr) {
-                const std::string s = *itr;
-                if (lastStr.find(s) != std::string::npos && s.length()) {
-                    //std::cout << "[+] KEY for string: " << lastStr << " found: " << s << "\n";
-                    return s; // the current string contains searched string
-                }
-            }
-            //std::cout << "[-] KEY for string: " << lastStr << " NOT found!\n";
-            return "";
-        }
+		//! Searches a given substring among the `watchedStrings`. If the substring found, return the corresponding watched string containing the substring. 
+		std::string hasWatchedSubstring(std::string& lastStr)
+		{
+			for (auto itr = watchedStrings.begin(); itr != watchedStrings.end(); ++itr) {
+				const std::string s = *itr;
+				if (lastStr.find(s) != std::string::npos && s.length()) {
+					//std::cout << "[+] KEY for string: " << lastStr << " found: " << s << "\n";
+					return s; // the current string contains searched string
+				}
+			}
+			//std::cout << "[-] KEY for string: " << lastStr << " NOT found!\n";
+			return "";
+		}
 
-        std::set<std::string> watchedStrings;
-    };
+		std::set<std::string> watchedStrings;
+	};
 
-    //! Statistics from a block of data
-    template <typename T>
-    struct ChunkStats {
-        //
-        ChunkStats()
-            : size(0), offset(0), entropy(0), longestStr(0), prevVal(0),
-            stringsCount(0), cleanStringsCount(0)
-        {
-        }
+	//! Statistics from a block of data
+	template <typename T>
+	struct ChunkStats {
+		//
+		ChunkStats()
+			: size(0), offset(0), entropy(0), longestStr(0), prevVal(0),
+			stringsCount(0), cleanStringsCount(0)
+		{
+		}
 
-        ChunkStats(size_t _offset, size_t _size)
-            : size(_size), offset(_offset), entropy(0), longestStr(0), prevVal(0),
-            stringsCount(0), cleanStringsCount(0)
-        {
-        }
+		ChunkStats(size_t _offset, size_t _size)
+			: size(_size), offset(_offset), entropy(0), longestStr(0), prevVal(0),
+			stringsCount(0), cleanStringsCount(0)
+		{
+		}
 
-        // Copy constructor
-        ChunkStats(const ChunkStats& p1)
-            : size(p1.size), offset(p1.offset),
-            entropy(p1.entropy), longestStr(p1.longestStr), lastStr(p1.lastStr), prevVal(p1.prevVal),
-            stringsCount(p1.stringsCount), cleanStringsCount(p1.cleanStringsCount)
-            histogram(p1.histogram),
-            frequencies(p1.frequencies),
-            settings(p1.settings), foundStrings(p1.foundStrings)
+		// Copy constructor
+		ChunkStats(const ChunkStats& p1)
+			: size(p1.size), offset(p1.offset),
+			entropy(p1.entropy), longestStr(p1.longestStr), lastStr(p1.lastStr), prevVal(p1.prevVal),
+			stringsCount(p1.stringsCount), cleanStringsCount(p1.cleanStringsCount)
+			histogram(p1.histogram),
+			frequencies(p1.frequencies),
+			settings(p1.settings), foundStrings(p1.foundStrings)
 
 #ifdef _KEEP_STR
-            , allStrings(p1.allStrings)
+			, allStrings(p1.allStrings)
 #endif //_KEEP_STR
-        {
-        }
+		{
+		}
 
-        void fillSettings(MultiStatsSettings* _settings)
-        {
-            settings = _settings;
-        }
+		void fillSettings(MultiStatsSettings* _settings)
+		{
+			settings = _settings;
+		}
 
-        void appendVal(T val)
-        {
+		void appendVal(T val)
+		{
 #ifdef SCAN_STRINGS
-            const bool isPrint = IS_PRINTABLE(val);
-            if (isPrint) {
-                lastStr += char(val);
-            }
-            else {
-                const bool isClean = (val == 0) ? true : false; //terminated cleanly?
-                finishLastStr(isClean);
-                lastStr.clear();
-            }
+			const bool isPrint = IS_PRINTABLE(val);
+			if (isPrint) {
+				lastStr += char(val);
+			}
+			else {
+				const bool isClean = (val == 0) ? true : false; //terminated cleanly?
+				finishLastStr(isClean);
+				lastStr.clear();
+			}
 
 #endif // SCAN_STRINGS
-            size++;
-            histogram[val]++;
-            prevVal = val;
-        }
+			size++;
+			histogram[val]++;
+			prevVal = val;
+		}
 
-        void finishLastStr(bool isClean)
-        {
-            if (lastStr.length() < 2) {
-                return;
-            }
-            stringsCount++;
-            if (isClean) cleanStringsCount++;
+		void finishLastStr(bool isClean)
+		{
+			if (lastStr.length() < 2) {
+				return;
+			}
+			stringsCount++;
+			if (isClean) cleanStringsCount++;
 
-            if (settings) {
-                std::string key = settings->hasWatchedSubstring(lastStr);
-                if (key.length()) {
-                    foundStrings[key]++; // the current string contains searched string
-                }
-            }
+			if (settings) {
+				std::string key = settings->hasWatchedSubstring(lastStr);
+				if (key.length()) {
+					foundStrings[key]++; // the current string contains searched string
+				}
+			}
 #ifdef _KEEP_STR
-            allStrings.push_back(lastStr);
+			allStrings.push_back(lastStr);
 #endif //_KEEP_STR
-            //std::cout << "-----> lastStr:" << lastStr << "\n";
-            if (lastStr.length() > longestStr) {
-                longestStr = lastStr.length();
-            }
-            lastStr.clear();
-        }
+			//std::cout << "-----> lastStr:" << lastStr << "\n";
+			if (lastStr.length() > longestStr) {
+				longestStr = lastStr.length();
+			}
+			lastStr.clear();
+		}
 
-        const virtual void fieldsToJSON(std::stringstream& outs, size_t level)
-        {
-            OUT_PADDED(outs, level, "\"offset\" : ");
-            outs << std::hex << "\"" << offset << "\"";
-            outs << ",\n";
-            OUT_PADDED(outs, level, "\"size\" : ");
-            outs << std::hex << "\"" << size << "\"";
-            outs << ",\n";
-            OUT_PADDED(outs, level, "\"charset_size\" : ");
-            outs << std::dec << histogram.size();
+		const virtual void fieldsToJSON(std::stringstream& outs, size_t level)
+		{
+			OUT_PADDED(outs, level, "\"offset\" : ");
+			outs << std::hex << "\"" << offset << "\"";
+			outs << ",\n";
+			OUT_PADDED(outs, level, "\"size\" : ");
+			outs << std::hex << "\"" << size << "\"";
+			outs << ",\n";
+			OUT_PADDED(outs, level, "\"charset_size\" : ");
+			outs << std::dec << histogram.size();
 
-            std::set<T> values;
-            size_t freq = getMostFrequentValues<T>(frequencies, values);
-            if (freq && values.size()) {
-                outs << ",\n";
-                OUT_PADDED(outs, level, "\"most_freq_vals\" : ");
-                outs << std::hex << "\"";
-                for (auto itr = values.begin(); itr != values.end(); ++itr) {
-                    T mVal = *itr;
-                    outs << hexdumpValue<BYTE>((BYTE*)&mVal, sizeof(T));
-                }
-                outs << "\"";
-            }
-            outs << ",\n";
-            OUT_PADDED(outs, level, "\"entropy\" : ");
-            outs << std::dec << entropy;
-        }
+			std::set<T> values;
+			size_t freq = getMostFrequentValues<T>(frequencies, values);
+			if (freq && values.size()) {
+				outs << ",\n";
+				OUT_PADDED(outs, level, "\"most_freq_vals\" : ");
+				outs << std::hex << "\"";
+				for (auto itr = values.begin(); itr != values.end(); ++itr) {
+					T mVal = *itr;
+					outs << hexdumpValue<BYTE>((BYTE*)&mVal, sizeof(T));
+				}
+				outs << "\"";
+			}
+			outs << ",\n";
+			OUT_PADDED(outs, level, "\"entropy\" : ");
+			outs << std::dec << entropy;
+		}
 
-        void summarize()
-        {
-            entropy = util::calcShannonEntropy(histogram, size);
-            finishLastStr(true);
+		void summarize()
+		{
+			entropy = util::calcShannonEntropy(histogram, size);
+			finishLastStr(true);
 
-            for (auto itr = histogram.begin(); itr != histogram.end(); ++itr) {
-                const size_t count = itr->second;
-                const  T val = itr->first;
-                frequencies[count].insert(val);
-            }
-        }
+			for (auto itr = histogram.begin(); itr != histogram.end(); ++itr) {
+				const size_t count = itr->second;
+				const  T val = itr->first;
+				frequencies[count].insert(val);
+			}
+		}
 
-        double entropy;
-        size_t size;
-        size_t offset;
+		double entropy;
+		size_t size;
+		size_t offset;
 
-        T prevVal;
-        size_t longestStr; // the longest ASCII string in the chunk
+		T prevVal;
+		size_t longestStr; // the longest ASCII string in the chunk
 
-        std::string lastStr;
-        size_t stringsCount;
-        size_t cleanStringsCount;
-        std::map<T, size_t> histogram;
-        std::map<size_t, std::set< T >>  frequencies;
+		std::string lastStr;
+		size_t stringsCount;
+		size_t cleanStringsCount;
+		std::map<T, size_t> histogram;
+		std::map<size_t, std::set< T >>  frequencies;
 
-        MultiStatsSettings *settings;
+		MultiStatsSettings *settings;
 
-        std::map<std::string, size_t> foundStrings;
+		std::map<std::string, size_t> foundStrings;
 #ifdef _KEEP_STR
-        std::vector< std::string > allStrings;
+		std::vector< std::string > allStrings;
 #endif
-    };
+	};
 
-    class AreaMultiStats : public AreaStats {
-    public:
-        AreaMultiStats()
-        {
-        }
+	class AreaMultiStats : public AreaStats {
+	public:
+		AreaMultiStats()
+		{
+		}
 
-        // Copy constructor
-        AreaMultiStats(const AreaMultiStats& p1)
-            : currArea(p1.currArea)
-        {
-        }
+		// Copy constructor
+		AreaMultiStats(const AreaMultiStats& p1)
+			: currArea(p1.currArea)
+		{
+		}
 
-        bool fillSettings(StatsSettings* settings)
-        {
-            MultiStatsSettings* multiSettings = dynamic_cast<MultiStatsSettings*>(settings);
-            if (!multiSettings) return false;
+		bool fillSettings(StatsSettings* settings)
+		{
+			MultiStatsSettings* multiSettings = dynamic_cast<MultiStatsSettings*>(settings);
+			if (!multiSettings) return false;
 
-            currArea.fillSettings(multiSettings);
-            return true;
-        }
+			currArea.fillSettings(multiSettings);
+			return true;
+		}
 
-        const virtual bool toJSON(std::stringstream& outs, size_t level)
-        {
-            OUT_PADDED(outs, level, "\"stats\" : {\n");
-            fieldsToJSON(outs, level + 1);
-            outs << "\n";
-            OUT_PADDED(outs, level, "}");
-            return true;
-        }
+		const virtual bool toJSON(std::stringstream& outs, size_t level)
+		{
+			OUT_PADDED(outs, level, "\"stats\" : {\n");
+			fieldsToJSON(outs, level + 1);
+			outs << "\n";
+			OUT_PADDED(outs, level, "}");
+			return true;
+		}
 
-        const virtual void fieldsToJSON(std::stringstream& outs, size_t level)
-        {
-            OUT_PADDED(outs, level, "\"full_area\" : {\n");
-            currArea.fieldsToJSON(outs, level + 1);
-            outs << "\n";
-            OUT_PADDED(outs, level, "}");
-        }
+		const virtual void fieldsToJSON(std::stringstream& outs, size_t level)
+		{
+			OUT_PADDED(outs, level, "\"full_area\" : {\n");
+			currArea.fieldsToJSON(outs, level + 1);
+			outs << "\n";
+			OUT_PADDED(outs, level, "}");
+		}
 
-        bool isFilled() const
-        {
-            return (currArea.size != 0) ? true : false;
-        }
+		bool isFilled() const
+		{
+			return (currArea.size != 0) ? true : false;
+		}
 
-        void summarize()
-        {
-            currArea.summarize();
-        }
+		void summarize()
+		{
+			currArea.summarize();
+		}
 
-        ChunkStats<BYTE> currArea; // stats from the whole area
+		ChunkStats<BYTE> currArea; // stats from the whole area
 
-    };
+	};
 };
--- stats/stats.h
+++ stats/stats.h
@@ -9,97 +9,97 @@
 
 namespace pesieve {
 
-    //! Settings defining what type of stats should be collected.
-    class StatsSettings {
-    public:
-        StatsSettings() : isFilled(false) {}
-    protected:
-        bool isFilled;
+	//! Settings defining what type of stats should be collected.
+	class StatsSettings {
+	public:
+		StatsSettings() : isFilled(false) {}
+	protected:
+		bool isFilled;
 
-    };
+	};
 
-    class AreaStats {
-    public:
-        AreaStats()
-            : area_start(0), area_size(0)
-        {
-        }
+	class AreaStats {
+	public:
+		AreaStats()
+			: area_start(0), area_size(0)
+		{
+		}
 
-        void setStartOffset(size_t _area_start)
-        {
-            area_start = _area_start;
-        }
+		void setStartOffset(size_t _area_start)
+		{
+			area_start = _area_start;
+		}
 
-        void appendVal(BYTE val)
-        {
-            _appendVal(val);
-            area_size++;
-        }
+		void appendVal(BYTE val)
+		{
+			_appendVal(val);
+			area_size++;
+		}
 
-        const virtual void fieldsToJSON(std::stringstream& outs, size_t level) = 0;
+		const virtual void fieldsToJSON(std::stringstream& outs, size_t level) = 0;
 
-        bool isFilled() const
-        {
-            return area_size > 0 ? true : false;
-        }
+		bool isFilled() const
+		{
+			return area_size > 0 ? true : false;
+		}
 
-        virtual void summarize() = 0;
+		virtual void summarize() = 0;
 
-        virtual bool fillSettings(StatsSettings* _settings) { return false; }
+		virtual bool fillSettings(StatsSettings* _settings) { return false; }
 
-        const virtual bool toJSON(std::stringstream& outs, size_t level)
-        {
-            if (!isFilled()) {
-                return false;
-            }
-            OUT_PADDED(outs, level, "\"stats\" : {\n");
-            fieldsToJSON(outs, level + 1);
-            outs << "\n";
-            OUT_PADDED(outs, level, "}");
-            return true;
-        }
+		const virtual bool toJSON(std::stringstream& outs, size_t level)
+		{
+			if (!isFilled()) {
+				return false;
+			}
+			OUT_PADDED(outs, level, "\"stats\" : {\n");
+			fieldsToJSON(outs, level + 1);
+			outs << "\n";
+			OUT_PADDED(outs, level, "}");
+			return true;
+		}
 
-    protected:
-        
-        virtual void _appendVal(BYTE val) = 0;
+	protected:
+		
+		virtual void _appendVal(BYTE val) = 0;
 
-        size_t area_size;
-        size_t area_start;
+		size_t area_size;
+		size_t area_start;
 
-        friend class AreaStatsCalculator;
+		friend class AreaStatsCalculator;
 
-    }; // AreaStats
+	}; // AreaStats
 
 
-    class AreaStatsCalculator {
-    public:
-        AreaStatsCalculator(const util::ByteBuffer& _buffer)
-            : buffer(_buffer)
-        {
-        }
+	class AreaStatsCalculator {
+	public:
+		AreaStatsCalculator(const util::ByteBuffer& _buffer)
+			: buffer(_buffer)
+		{
+		}
 
-        bool fill(AreaStats& stats, StatsSettings* settings)
-        {
-            const bool skipPadding = true;
-            const size_t data_size = buffer.getDataSize(skipPadding);
-            const BYTE* data_buf = buffer.getData(skipPadding);
-            if (!data_size || !data_buf) {
-                return false;
-            }
-           
-            stats.fillSettings(settings);
-            stats.setStartOffset(buffer.getStartOffset(skipPadding));
-            BYTE lastVal = 0;
-            for (size_t i = 0; i < data_size; ++i) {
-                const BYTE val = data_buf[i];
-                stats.appendVal(val);
-            }
-            stats.summarize();
-            return true;
-        }
+		bool fill(AreaStats& stats, StatsSettings* settings)
+		{
+			const bool skipPadding = true;
+			const size_t data_size = buffer.getDataSize(skipPadding);
+			const BYTE* data_buf = buffer.getData(skipPadding);
+			if (!data_size || !data_buf) {
+				return false;
+			}
+		   
+			stats.fillSettings(settings);
+			stats.setStartOffset(buffer.getStartOffset(skipPadding));
+			BYTE lastVal = 0;
+			for (size_t i = 0; i < data_size; ++i) {
+				const BYTE val = data_buf[i];
+				stats.appendVal(val);
+			}
+			stats.summarize();
+			return true;
+		}
 
-    private:
-        const util::ByteBuffer& buffer;
-    };
+	private:
+		const util::ByteBuffer& buffer;
+	};
 
 }; //pesieve
