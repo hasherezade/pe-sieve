04ad528065ae75757f4b67cdc7c58659ee5b94b5
hasherezade <hasherezade@users.noreply.github.com>
2023-08-29 20:23:37 +0000

    deploy: b2333ff583430b61435d31886583f74a26c10dc8

--- search/namespaces_1.js
+++ search/namespaces_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['pesieve_0',['pesieve',['../namespacepesieve.html',1,'']]],
-  ['stats_1',['stats',['../namespacepesieve_1_1stats.html',1,'pesieve']]],
-  ['util_2',['util',['../namespacepesieve_1_1util.html',1,'pesieve']]]
+  ['pesieve_3a_3astats_1',['stats',['../namespacepesieve_1_1stats.html',1,'pesieve']]],
+  ['pesieve_3a_3autil_2',['util',['../namespacepesieve_1_1util.html',1,'pesieve']]]
 ];
---
+++ search/pages_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['bindings_0',['Available bindings',['../index.html',1,'']]]
+];
