1929559f6640af565b0552c5f5e406adee60d75a
hasherezade <hasherezade@users.noreply.github.com>
2024-02-15 22:03:56 +0000

    deploy: 76ff351351815ea2316c166f20a4c3f1331a3940

--- search/variables_8.js
+++ search/variables_8.js
@@ -1,19 +1,18 @@
 var searchData=
 [
   ['hardcodedpatterns_0',['HardcodedPatterns',['../namespacepesieve.html#afd37518d5bc5a27fc935b3b156f342ea',1,'pesieve']]],
-  ['has_5fpatterns_1',['has_patterns',['../classpesieve_1_1_working_set_scan_report.html#a8e70bced852069d9d3840138944a1735',1,'pesieve::WorkingSetScanReport']]],
-  ['has_5fpe_2',['has_pe',['../classpesieve_1_1_working_set_scan_report.html#aa07022105bbf1530e5fe9f7b0f9c4b71',1,'pesieve::WorkingSetScanReport']]],
-  ['has_5fshellcode_3',['has_shellcode',['../classpesieve_1_1_working_set_scan_report.html#aae424b26aea4d4149da47f74a033b2e6',1,'pesieve::WorkingSetScanReport']]],
-  ['hdr_5fmod_4',['hdr_mod',['../structreport.html#a87d7a61161d8ca92757893e667c320bb',1,'report']]],
-  ['headerbuffer_5',['headerBuffer',['../classpesieve_1_1_remote_module_data.html#a601a27ff576179474ff6270d4bdbc87f',1,'pesieve::RemoteModuleData']]],
-  ['hfile_6',['hFile',['../namespacepesieve_1_1util.html#abbfc80ee34aa4b5e1dfa78c1537fe405',1,'pesieve::util']]],
-  ['hilighted_5fcolor_7',['HILIGHTED_COLOR',['../namespacepesieve.html#a1eba647f3f36f78d8280a93989228576',1,'pesieve']]],
-  ['histogram_8',['histogram',['../classpesieve_1_1_area_entropy_stats.html#a6fba37441b9fc3f27ba4ddb046012cf5',1,'pesieve::AreaEntropyStats::histogram'],['../structpesieve_1_1_chunk_stats.html#ac86682199b912201e57117ad9af87a4e',1,'pesieve::ChunkStats::histogram']]],
-  ['hooked_5ffunc_9',['hooked_func',['../classpesieve_1_1_patch_list_1_1_patch.html#a4aa1cbe33dae22e9e11bcbaf2e78331f',1,'pesieve::PatchList::Patch']]],
-  ['hookstagfilename_10',['hooksTagFileName',['../classpesieve_1_1_module_dump_report.html#a8c3e80abf3b10c812816630052e02e5c',1,'pesieve::ModuleDumpReport']]],
-  ['hooktargetmodname_11',['hookTargetModName',['../classpesieve_1_1_patch_list_1_1_patch.html#a539b13c352084f0c62380fd35f388d83',1,'pesieve::PatchList::Patch']]],
-  ['hooktargetmodule_12',['hookTargetModule',['../classpesieve_1_1_patch_list_1_1_patch.html#acec7a5d8080e88233a3df43d8baf22b5',1,'pesieve::PatchList::Patch']]],
-  ['hooktargetva_13',['hookTargetVA',['../classpesieve_1_1_patch_list_1_1_patch.html#a88a970b68970e94884d5f4e2efd80359',1,'pesieve::PatchList::Patch']]],
-  ['hprocess_14',['hProcess',['../struct__t__stack__enum__params.html#a657834de8b3a9b7525907db521be6552',1,'_t_stack_enum_params']]],
-  ['hthread_15',['hThread',['../struct__t__stack__enum__params.html#af39d9900e3d580fea3a16440f979bcef',1,'_t_stack_enum_params']]]
+  ['has_5fpe_1',['has_pe',['../classpesieve_1_1_working_set_scan_report.html#aa07022105bbf1530e5fe9f7b0f9c4b71',1,'pesieve::WorkingSetScanReport']]],
+  ['has_5fshellcode_2',['has_shellcode',['../classpesieve_1_1_working_set_scan_report.html#aae424b26aea4d4149da47f74a033b2e6',1,'pesieve::WorkingSetScanReport']]],
+  ['hdr_5fmod_3',['hdr_mod',['../structreport.html#a87d7a61161d8ca92757893e667c320bb',1,'report']]],
+  ['headerbuffer_4',['headerBuffer',['../classpesieve_1_1_remote_module_data.html#a601a27ff576179474ff6270d4bdbc87f',1,'pesieve::RemoteModuleData']]],
+  ['hfile_5',['hFile',['../namespacepesieve_1_1util.html#abbfc80ee34aa4b5e1dfa78c1537fe405',1,'pesieve::util']]],
+  ['hilighted_5fcolor_6',['HILIGHTED_COLOR',['../namespacepesieve.html#a1eba647f3f36f78d8280a93989228576',1,'pesieve']]],
+  ['histogram_7',['histogram',['../classpesieve_1_1_area_entropy_stats.html#a6fba37441b9fc3f27ba4ddb046012cf5',1,'pesieve::AreaEntropyStats::histogram'],['../structpesieve_1_1_chunk_stats.html#ac86682199b912201e57117ad9af87a4e',1,'pesieve::ChunkStats::histogram']]],
+  ['hooked_5ffunc_8',['hooked_func',['../classpesieve_1_1_patch_list_1_1_patch.html#a4aa1cbe33dae22e9e11bcbaf2e78331f',1,'pesieve::PatchList::Patch']]],
+  ['hookstagfilename_9',['hooksTagFileName',['../classpesieve_1_1_module_dump_report.html#a8c3e80abf3b10c812816630052e02e5c',1,'pesieve::ModuleDumpReport']]],
+  ['hooktargetmodname_10',['hookTargetModName',['../classpesieve_1_1_patch_list_1_1_patch.html#a539b13c352084f0c62380fd35f388d83',1,'pesieve::PatchList::Patch']]],
+  ['hooktargetmodule_11',['hookTargetModule',['../classpesieve_1_1_patch_list_1_1_patch.html#acec7a5d8080e88233a3df43d8baf22b5',1,'pesieve::PatchList::Patch']]],
+  ['hooktargetva_12',['hookTargetVA',['../classpesieve_1_1_patch_list_1_1_patch.html#a88a970b68970e94884d5f4e2efd80359',1,'pesieve::PatchList::Patch']]],
+  ['hprocess_13',['hProcess',['../struct__t__stack__enum__params.html#a657834de8b3a9b7525907db521be6552',1,'_t_stack_enum_params']]],
+  ['hthread_14',['hThread',['../struct__t__stack__enum__params.html#af39d9900e3d580fea3a16440f979bcef',1,'_t_stack_enum_params']]]
 ];
