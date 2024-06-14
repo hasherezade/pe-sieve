c1ebb813dc2cccb05af3fd8cc39a38191258a83d
hasherezade <hasherezade@users.noreply.github.com>
2024-06-14 14:19:27 +0000

    deploy: 6805ada9be6ff8f39426fd49d184fbd7f77721a6

--- search/all_7.js
+++ search/all_7.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['g_5fhardcodedpatternsmutex_0',['g_HardcodedPatternsMutex',['../namespacepesieve.html#aa7349a31e870d8fe9717470c3b4a0e96',1,'pesieve']]],
   ['g_5fkernel32hndl_1',['g_kernel32Hndl',['../namespacepesieve_1_1util.html#aacb64ab7d443d809726bde821ffd5cdc',1,'pesieve::util']]],
-  ['g_5fmainmatchermutex_2',['g_mainMatcherMutex',['../artefacts__util_8cpp.html#a3ad404666a99e16e91225e8735d6b164',1,'artefacts_util.cpp']]],
+  ['g_5fmatcher_2',['g_Matcher',['../pe__sieve_8cpp.html#a4adee9c78e4181a83b0dc7323334ed9d',1,'g_Matcher:&#160;pe_sieve.cpp'],['../workingset__scanner_8cpp.html#a4adee9c78e4181a83b0dc7323334ed9d',1,'g_Matcher:&#160;pe_sieve.cpp']]],
   ['g_5fsystem32path_3',['g_System32Path',['../path__converter_8cpp.html#ae7c65d5a1eb715d175fb5653863e90c2',1,'path_converter.cpp']]],
   ['g_5fsyswow64path_4',['g_Syswow64Path',['../path__converter_8cpp.html#a8ad45dd38e2bdd77c4eceb801cb1492e',1,'path_converter.cpp']]],
   ['generateartefacts_5',['generateArtefacts',['../classpesieve_1_1_artefact_scanner.html#af8d958891fe3a11a6a6b5f63f85e1bd5',1,'pesieve::ArtefactScanner']]],
--- search/namespaces_1.js
+++ search/namespaces_1.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['pesieve_0',['pesieve',['../namespacepesieve.html',1,'']]],
-  ['pesieve_3a_3amatcher_1',['matcher',['../namespacepesieve_1_1matcher.html',1,'pesieve']]],
-  ['pesieve_3a_3astats_2',['stats',['../namespacepesieve_1_1stats.html',1,'pesieve']]],
-  ['pesieve_3a_3autil_3',['util',['../namespacepesieve_1_1util.html',1,'pesieve']]]
+  ['pesieve_3a_3astats_1',['stats',['../namespacepesieve_1_1stats.html',1,'pesieve']]],
+  ['pesieve_3a_3autil_2',['util',['../namespacepesieve_1_1util.html',1,'pesieve']]]
 ];
