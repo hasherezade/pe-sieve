d21fc19506601ace4d06eb72b311a563ff4abf2e
hasherezade <hasherezade@users.noreply.github.com>
2026-04-01 17:51:36 +0000

    deploy: e278e769715457fe2c3373d47bc6d8ebe95a7afe

--- search/variables_a.js
+++ search/variables_a.js
@@ -7,6 +7,5 @@ var searchData=
   ['json_5flevel_4',['JSON_LEVEL',['../classpesieve_1_1_pe_artefacts.html#adb4622c722afba90a69fc5ff56884ee7',1,'pesieve::PeArtefacts::JSON_LEVEL'],['../classpesieve_1_1_element_scan_report.html#a2fc362050f25eeb5ba6c02769aa036de',1,'pesieve::ElementScanReport::JSON_LEVEL']]],
   ['json_5flvl_5',['json_lvl',['../structparams.html#a8612591e62e039621e80ceeae630cfd4',1,'params::json_lvl'],['../namespacedemo.html#a0f33e043da52b5fc4d36a354563f7ccc',1,'demo.json_lvl']]],
   ['json_5flvl_5fcount_6',['JSON_LVL_COUNT',['../classpesieve_1_1t__json__level.html#a1ee2ba9e62b25348e0797d705d795f36',1,'pesieve::t_json_level']]],
-  ['json_5fmax_5fsize_7',['json_max_size',['../namespacedemo.html#acbe434c91aa2d7109df72eea6e6edd9b',1,'demo']]],
-  ['json_5foutput_8',['json_output',['../structparams.html#af64777f36a67761e327b943ab53fd797',1,'params::json_output'],['../namespacedemo.html#ac51fee34c29c684edf9d8bb6247ce9c6',1,'demo.json_output']]]
+  ['json_5foutput_7',['json_output',['../structparams.html#af64777f36a67761e327b943ab53fd797',1,'params::json_output'],['../namespacedemo.html#ac51fee34c29c684edf9d8bb6247ce9c6',1,'demo.json_output']]]
 ];
